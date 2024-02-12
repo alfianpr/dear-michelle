4a44307bd09aaca64cece5dd76bb4fc9dafef70f
alfianpr <alfian.pratama@kalbenutritionals.com>
2024-02-12 19:58:32 +0700

    push

---
+++ js/yes.js
@@ -0,0 +1,28 @@
+var popup = "Dear Michelle, \n"
+popup += "\n"
+popup += "As Valentine's Day draws near, my heart feels compelled to express the depths of my affection for you. You are the embodiment of everything I've ever hoped for in a partner - your kindness, intelligence, and unwavering support have enriched my life in ways I never thought possible. \n"
+popup += "\n"
+popup += "Yet, as this day of love approaches, I find myself grappling with my own hesitations and uncertainties. I want you to know that my struggle to reach commitment has nothing to do with my feelings for you. If anything, it's a testament to just how deeply I care for you and how profoundly I want to get it right. \n"
+popup += "\n"
+popup += "The truth is, my love, you mean everything to me. Every moment we share is etched into the fabric of my soul, and the thought of a future without you is unimaginable. You've shown me what it means to be truly loved, and for that, I am endlessly grateful."
+popup += "\n"
+popup += "But as much as I long to take that leap of faith and commit to you fully, I find myself held back by my own fears and insecurities. I worry that I may not be able to give you the love and support you deserve, or that I may somehow let you down. \n"
+popup += "\n"
+popup += "Yet, despite these doubts, one thing remains abundantly clear - I am willing to fight for us, to confront my fears head-on, and to work tirelessly towards building a future together. Because with you, my love, I see endless possibilities, endless potential for happiness and fulfillment. \n"
+popup += "\n"
+popup += "So, on this Valentine's Day, I want to make you a promise - a promise to be honest and open with you, to communicate my fears and insecurities, and to work hand in hand with you to overcome any obstacles that stand in our way. \n"
+popup += "\n"
+popup += "I may be struggling to reach commitment, but I want you to know that my love for you is unwavering. You are my rock, my guiding light, and the love of my life, and I am committed to doing whatever it takes to make our relationship thrive. \n"
+popup += "\n"
+popup += "Happy Valentine's Day, my dear. Here's to us - to love, to growth, and to the journey that lies ahead. \n"
+popup += "\n"
+popup += "With all my love, \n"
+popup += "\n"
+popup += "Alfian"
+
+
+document.getElementById('Button1').addEventListener('click', function() {
+alert(
+    popup
+);
+});
